0c82431ad3a9b1fd3da3788b0e53dd68512dc344
nathalia-goncalves <00001125017740sp@al.sp.gov.br>
2025-04-29 09:43:16 -0300

    sistema de notas

---
+++ python.py
@@ -0,0 +1,8 @@
+print("sistema de notas")
+
+nota_01 = int(input("Digite a perimeira nota"))
+nota_02 = int(input("Digite a segunda nota"))
+nota_03 = int(input("Digite a terceira nota"))
+nota_04 = int(input("Digite a quarta nota"))
+
+soma = nota_01 + nota_02 + nota_03 + nota_04
