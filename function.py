55fcc2ea299033a6a1fdf60a9c4d22087d0d8460
nathalia-goncalves <00001125017740sp@al.sp.gov.br>
2025-05-05 13:34:36 -0300

    funciotion.py

---
+++ function.py
@@ -0,0 +1,27 @@
+def multiplicação():
+    num_01 = 25
+    num_02 = 31
+
+    resultado = num_01 + num_02
+
+    return resultado
+
+def multiplicarDoisNumeros(nota_01, nota_02):
+    num_01 = nota_01
+    num_02 = nota_02
+
+    resultado = nota_01 * nota_02
+
+    return print(resultado)
+
+multiplicarDoisNumeros(24, 98)    
+
+def cadastrar(nome,idade):
+    data = {
+    "nome": nome,
+     "idade": idade,
+     }
+    return print(data)
+cadastrar("nathalia", 16)
+
+ 
